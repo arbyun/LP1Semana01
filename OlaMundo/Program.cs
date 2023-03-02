afcea9ad143c5ac471d98e913c081c05488566e1
Daniela Castro <someonealiveh@gmail.com>
2023-03-02 11:52:51 +0000

    Added documentation on the args for the Main function

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -4,6 +4,14 @@ namespace OlaMundo
 {
     class Program
     {
+        /// <summary>
+        /// Possible arguments to pass on the Main function
+        /// </summary>
+        /// <param name="args">
+        /// arg1: any string,
+        /// arg2: any string,
+        /// ...so on</param>
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
