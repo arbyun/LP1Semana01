67ce25041f9ab19df160e3bc22b4eff61352a5f0
Daniela Castro <someonealiveh@gmail.com>
2023-03-02 11:44:36 +0000

    Added the Argumentos solution!

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World! :)");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Argument {i+1}");
+            }
+        }
+    }
+}
